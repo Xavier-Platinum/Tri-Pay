0827c445df3667ad822b1d2296e2dde6a3a6970b
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-04-07 15:45:24 +0100

    updated controllers

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+export {
+    create_payment,
+    flutterwave_verify_payment,
+    paystack_verify_payment
+} from "./payment/payment.controller";
--- app/controllers/payment/payment.controller.js
+++ app/controllers/payment/payment.controller.js
@@ -1,44 +1,106 @@
-import Transaction from "../../models";
-import {charge_ng_acct, create_payment} from "../../utils/helpers/flutterwave";
+import Transaction from "../../models/Payments/Payment.model";
+import {createPayment} from "../../utils/helpers/flutterwave";
 import {initializePayment, verifyPayment} from "../../utils/helpers/paystack";
 import crypto from "crypto";
-import {v5 as uuid} from "uuid";
+import Status from "../../models/Init/Status";
 
-exports.flutterwave_create_payment = async(req, res, next) => {
-    const {name, email, amount} = req.body;
-    if(!name || !email || !amount) {
-        return res.json({msg: "Invalid Body"})
-    }
 
-    // check if the amount coming from body is less than ₦500
-    if (parseInt(amount) < 50) {
-        return res.json({msg: "Amount must be ₦500"});
-    }
-    const tx_ref = uuid("Payment_id", uuid.URL);
-    let {data} = await create_payment(name, email, amount, tx_ref)
-    let transaction_payload = {
-        amount,
-        reference: tx_ref,
-        name,
-        email,
-        status: "pending",
-        payment_gateway: "flutter-wave"
-    }
-    await Transaction.create(transaction_payload)
-    .then(async(response) => {
-        return res.status(301).json({
-            message: "Saved",
-            response,
-            redirect_link: data.link
-        })
+export const create_payment = async(req, res, next) => {
+    await Status.find({name: "paystack"})
+    .then(async(result) => {
+        if (result.status_code === 503) {
+            const {name, email, amount} = req.body;
+            if(!name || !email || !amount) {
+                return res.json({msg: "Invalid Body"})
+            }
+
+            // check if the amount coming from body is less than ₦500
+            if (parseInt(amount) < 50) {
+                return res.json({msg: "Amount must be ₦500"});
+            }
+
+            const flutterwave_data = {
+                tx_ref: crypto.randomBytes(4).toString("hex"),
+                amount,
+                currency: "NGN",
+                redirect_url: "http://localhost:5000/flutterwave/verify",
+                customer: {
+                    email,
+                    name
+                }, 
+                customizations: {
+                    title: "Tri_Pay"
+                }
+            }
+
+            let {data} = await createPayment(flutterwave_data);
+            let transaction_payload = {
+                amount,
+                reference: tx_ref,
+                name,
+                email,
+                status: "pending",
+                payment_gateway: "flutter-wave"
+            }
+            await Transaction.create(transaction_payload)
+            .then(async(response) => {
+                return res.status(301).json({
+                    message: "Saved",
+                    response,
+                    redirect_link: data.link
+                })
+            })
+            .catch((err) => {
+                return res.status(400).json({err})
+            })
+        } else {
+            const {name, email, amount} = req.body;
+            if (!name || !email || !amount) {
+                return res.json({msg: "All fields are required"})
+            }
+        
+            // check if the amount coming from body is less than ₦500
+            if (parseInt(amount) < 50) {
+                return res.json({msg: "Amount must be ₦500"});
+            }
+        
+            // payload to send to paystack to initialize a transaction
+            const paystack_data = {
+                amount: parseInt(amount) * 100,
+                email: email,
+                reference: crypto.randomBytes(4).toString("hex"),
+            };
+        
+            let payment_gateway_response = await initializePayment(paystack_data);
+        
+            let transation_payload = {
+                amount: parseInt(amount),
+                status: "pending",
+                reference: paystack_data.reference,
+                access_code: payment_gateway_response.data.access_code,
+                payment_gateway: "paystack"
+            };
+        
+            await Transaction.create(transation_payload)
+            .then(async(response) => {
+                return res.status(301).json({
+                    message: "Saved",
+                    response,
+                    redirect_link: payment_gateway_response.data.authorization_url
+                })
+            })
+            .catch((err) => {
+                return res.status(400).json({err})
+            })
+        }
     })
     .catch((err) => {
-        return res.status(400).json({err})
+        res.json({msg: "No response recorded"})
     })
-    // if (gate_response) return (301, gate_response.data.link)
+    
 } 
 
-exports.flutterwave_verify_payment = async(req, res, next) => {
+export const flutterwave_verify_payment = async(req, res, next) => {
     if (req.query.status === 'successful') {
         const transactionDetails = await Transaction.find({ref: req.query.tx_ref});
         const response = await flw.Transaction.verify({id: req.query.transaction_id});
@@ -57,48 +119,7 @@ exports.flutterwave_verify_payment = async(req, res, next) => {
     }
 }
 
-exports.paystack_create_payment = async(req, res, next) => {
-    const {name, email, amount} = req.body;
-    if (!name || !email || !amount) {
-        return res.json({msg: "All fields are required"})
-    }
-
-    // check if the amount coming from body is less than ₦500
-    if (parseInt(amount) < 50) {
-        return res.json({msg: "Amount must be ₦500"});
-    }
-
-    // payload to send to paystack to initialize a transaction
-    const paystack_data = {
-        amount: parseInt(amount) * 100,
-        email: email,
-        reference: crypto.randomBytes(4).toString("hex"),
-    };
-
-    let payment_gateway_response = await initializePayment(paystack_data);
-
-    let transation_payload = {
-        amount: parseInt(amount),
-        status: "pending",
-        reference: paystack_data.reference,
-        access_code: payment_gateway_response.data.access_code,
-        payment_gateway: "paystack"
-    };
-
-    await Transaction.create(transation_payload)
-    .then(async(response) => {
-        return res.status(301).json({
-            message: "Saved",
-            response,
-            redirect_link: payment_gateway_response.data.authorization_url
-        })
-    })
-    .catch((err) => {
-        return res.status(400).json({err})
-    })
-}
-
-exports.paystack_verify_payment = async(req, res, next) => {
+export const paystack_verify_payment = async(req, res, next) => {
     const { trxref } = req.query;
 
     if (!trxref) {
