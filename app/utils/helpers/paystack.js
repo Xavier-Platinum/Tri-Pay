f96ea9ba54fc9528702411505d273fc7ae0377cb
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-04-07 15:46:49 +0100

    testing update

--- app/utils/helpers/paystack.js
+++ app/utils/helpers/paystack.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-/**
- * Initialize paystack transaction. create an Object with all the params below included in it.
- * @param {Number} amount (required).
- * @param {String} email Logged in user's email (required).
- * @param {String} reference A random number generated to track transactions (optional).
- * @returns {Object} this.data.authorization_url. The URL the payment will be made.
- */
-
 exports.initializePayment = async (form) => {
     const options = {
         url: "https://api.paystack.co/transaction/initialize",
@@ -28,12 +20,6 @@ exports.initializePayment = async (form) => {
         }
     });
 };
-
-/**
- * Verify all transactions before updating their status in the DB
- * @param {String} trxref The reference String to verify the transaction. It will be gotten after successfully
- * initializing a transaction.
- */
 
 exports.verifyPayment = async (ref) => {
     const options = {
