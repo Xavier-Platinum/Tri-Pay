fc8fb63686f06769b1769e8a4298c594ccce0368
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-04-07 22:56:02 +0100

    Test complete

--- app/middleware/jobs/job.js
+++ app/middleware/jobs/job.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import {paystack_availability, flutterwave_availability} from "./statusCheck.js";
 // checking for availibility of service
-cron.schedule("5 * * * * *", () => {
+cron.schedule("*/300 * * * * *", () => {
     console.log("============ Checking now ===============");
     paystack_availability()
     flutterwave_availability()
